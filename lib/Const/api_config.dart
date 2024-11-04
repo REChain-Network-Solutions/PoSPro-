e2eea1fe1802d3e32d430e108d25608959e25ba2
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2024-11-04 21:41:25 +0300

    Update api_config.dart

--- lib/Const/api_config.dart
+++ lib/Const/api_config.dart
@@ -1,6 +1,6 @@
 class APIConfig {
-  static String domain = 'https://modus.delus.site/';
-  static String url = 'https://modus.delus.site/api/v1';
+  static String domain = 'https://pospro.acnoo.com/';
+  static String url = 'https://pospro.acnoo.com/api/v1';
   static String registerUrl = '/sign-up';
   static String businessCategoriesUrl = '/business-categories';
 }
